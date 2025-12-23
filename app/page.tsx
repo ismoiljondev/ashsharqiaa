b7ab72efdb1ff0efbcc5ea26733d44e8ebc4ced8
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2025-12-23 17:54:26 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-center">Comming soon...</p>
+    </div>
+  );
+}
