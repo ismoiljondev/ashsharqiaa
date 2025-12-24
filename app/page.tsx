8d4ce2a1c9386dd90b80577c0f21134961168edd
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2025-12-24 16:23:59 +0500

    feat shadcn and variable installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <p className="text-center">Comming soon...</p>
+      <Button>Submit</Button>
     </div>
   );
 }
